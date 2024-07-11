8993a7a363294c6dd39b018a73742566998f5adc
Yasmeen-khan2004 <yasmeen789144@email.com>
2024-07-11 18:00:39 +0530

    third time

--- indic_translator.py
+++ indic_translator.py
@@ -13,7 +13,7 @@ class TranslatorApp:
         self.create_widgets()
 
     def get_language_list(self):
-        indian_languages =  ["en","hi", "bn", "gu", "kn", "km", "gom", "ml", "mni-Mtei", "mr", "ne", "or", "pa", "sa", "sd", "ta","te", "ur", "bho", "sw", "mai", "doi"] 
+        indian_languages =  ["en","hi", "bo", "gu", "kn", "km", "gom", "ml", "mni-Mtei", "mr", "ne", "or", "pa", "sa", "sd", "ta","te", "ur", "bho", "sw", "mai", "doi"] 
 # Example: 1) Assamese, (2) Bengali, (3) Gujarati, (4) Hindi, (5) Kannada, (6) Kashmiri, (7) Konkani, (8) Malayalam, (9) Manipuri, (10) Marathi, (11) Nepali, (12) Oriya, (13) Punjabi, (14) Sanskrit, (15) Sindhi, (16) Tamil, (17) Telugu, (18) Urdu (19) Bodo, (20) Santhali, (21) Maithili and (22) Dogri.
 
 
